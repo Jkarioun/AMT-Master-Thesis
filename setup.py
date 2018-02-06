27fde03cf768a96db7da9fae18578caf8da6986e
simoons <simon.tihon@student.uclouvain.be>
2018-02-06 17:48:39 +0100

    Copy paste of magenta model

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='amt-master-thesis',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Jaafar and Simon',
+    author_email='',
+    description=''
+)
